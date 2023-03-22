5d2f5003268628b035793d74aa2d858567c88acc
dsibi <dasibirin@gmail.com>
2023-03-22 18:21:46 +0300

    add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="collapse navbar-collapse custom-navmenu" id="main-navbar">
+        <div className="container py-2 py-md-5">
+          {/* style="text-align: end" */}
+          <ul className="custom-menu">
+            <li className="active"><a href="index.html">Home</a></li>
+            <li><a href="about.html">About Me</a></li>
+          </ul>
+        </div>
+      </div>
+      <nav className="navbar navbar-light custom-navbar">
+        <div className="container">
+          {/* <a href="index.html"><img className="img-fluid" src="img/favicon/android-chrome-192x192.png" width="80px"></a> */}
+          <a href="#" className="burger" data-toggle="collapse" data-target="#main-navbar">
+            <span></span>
+          </a>
+        </div>
+      </nav>
+    </>
+
   );
 }
 
